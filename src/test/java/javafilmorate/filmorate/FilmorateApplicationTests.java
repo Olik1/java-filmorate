e32c6b6fbbea44d349daf1216ec9e81deef4e640
Olik1 <Dorofeeva86@yandex.ru>
2023-03-17 10:01:02 +0300

    template for project

---
+++ src/test/java/javafilmorate/filmorate/FilmorateApplicationTests.java
@@ -0,0 +1,13 @@
+package javafilmorate.filmorate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmorateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
