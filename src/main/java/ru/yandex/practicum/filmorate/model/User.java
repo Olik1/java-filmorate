0a9b5ec097c4d2412db4b813ede37506d8489080
Olik1 <Dorofeeva86@yandex.ru>
2023-03-24 13:33:32 +0300

    refactor: add builder for tests

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,11 +1,13 @@
 package ru.yandex.practicum.filmorate.model;
 
+import lombok.Builder;
 import lombok.Data;
 
 import javax.validation.constraints.*;
 import java.time.LocalDate;
 
 @Data
+@Builder
 public class User {
 
     private int id;
