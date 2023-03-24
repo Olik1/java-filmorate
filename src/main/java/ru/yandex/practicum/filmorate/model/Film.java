08d75aa66ef5a61ad6e0aa6660b25ec6e511a548
Olik1 <Dorofeeva86@yandex.ru>
2023-03-24 13:31:57 +0300

    refactor: add builder

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,16 +1,18 @@
 package ru.yandex.practicum.filmorate.model;
 
+import lombok.Builder;
 import lombok.Data;
 
 import javax.validation.constraints.*;
 import java.time.LocalDate;
 
 @Data
+@Builder
 public class Film {
-    private int id;
 
+    private int id;
     @NotBlank()
-    @Size(max=200)
+    @Size(max = 200)
     private String name;
     @NotEmpty
     @NotNull
