5835c264919a9189934a1c65b90eeeb452d7175d
Olga <110121612+Olik1@users.noreply.github.com>
2023-04-08 16:10:43 +0300

    refactor: remove method to User

--- src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
@@ -3,6 +3,7 @@ package ru.yandex.practicum.filmorate.storage;
 import ru.yandex.practicum.filmorate.model.User;
 
 import java.util.List;
+import java.util.Set;
 
 public interface UserStorage {
     User addUser(User user);
@@ -12,5 +13,6 @@ public interface UserStorage {
     List<User> getUserList();
 
     void deleteAllUsers();
+    Set<Integer> getAllId();
 
 }
