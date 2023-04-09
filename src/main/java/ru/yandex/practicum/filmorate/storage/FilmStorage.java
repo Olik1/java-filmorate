220d4df3939a9281d4538e54b32dece69bbfca47
Olga <110121612+Olik1@users.noreply.github.com>
2023-04-09 20:30:06 +0300

    refactor: добавление метода по получению айди

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -3,10 +3,12 @@ package ru.yandex.practicum.filmorate.storage;
 import ru.yandex.practicum.filmorate.model.Film;
 
 import java.util.List;
+import java.util.Set;
 
 public interface FilmStorage {
-    Film addFilm(Film film);
+    Film save(Film film);
     Film findFilmById(int id);
     List<Film> getFilmList();
     void deleteAllFilms();
+    Set<Integer> getAllId();
 }
