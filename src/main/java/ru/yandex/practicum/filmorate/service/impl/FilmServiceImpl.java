f161943c1ed1686852f4ac8af9493e0d92dbfe20
Olga <110121612+Olik1@users.noreply.github.com>
2023-04-09 20:30:52 +0300

    refactor: добавление методов по работе с лайками

--- src/main/java/ru/yandex/practicum/filmorate/service/impl/FilmServiceImpl.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/impl/FilmServiceImpl.java
@@ -1,37 +1,43 @@
 package ru.yandex.practicum.filmorate.service.impl;
 
+import ru.yandex.practicum.filmorate.exception.ObjectNotFoundException;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.service.FilmService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.storage.FilmStorage;
+import ru.yandex.practicum.filmorate.storage.UserStorage;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Comparator;
 import java.util.List;
-import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 @Slf4j
 public class FilmServiceImpl implements FilmService {
     private static int id;
     private static final LocalDate DATE = LocalDate.of(1895, 12, 28);
-    private final Map<Integer, Film> films = new HashMap<>();
+    FilmStorage filmStorage;
+    UserStorage userStorage;
+
 
     @Override
     public Film createFilm(Film film) {
         validateFilm(film);
         film.setId(generateFilmId());
-        films.put(film.getId(), film);
+        filmStorage.save(film);
         return film;
     }
 
     @Override
     public Film updateFilm(Film film) {
-        if (films.containsKey(film.getId())) {
+        if (filmStorage.getAllId().contains(film.getId())) {
             validateFilm(film);
-            films.put(film.getId(), film);
+            filmStorage.save(film);
         } else {
             log.error("ERROR: ID введен неверно - такого фильма не существует!");
             throw new ValidationException("Такого фильма не существует!");
@@ -42,7 +48,35 @@ public class FilmServiceImpl implements FilmService {
 
     @Override
     public List<Film> getAllFilms() {
-        return new ArrayList<>(films.values());
+        return new ArrayList<>(filmStorage.getFilmList());
+    }
+
+    @Override
+    public void addLike(int userId, int filmId) {
+        if (filmStorage.findFilmById(userId) == null || filmStorage.findFilmById(filmId) == null) {
+            throw new ObjectNotFoundException("Film doesn't found");
+        }
+        User user = userStorage.findUserById(userId);
+        Film film = filmStorage.findFilmById(filmId);
+        film.addLike(user);
+    }
+
+    @Override
+    public void deleteLike(int userId, int filmId) {
+        if (filmStorage.findFilmById(userId) == null || filmStorage.findFilmById(filmId) == null) {
+            throw new ObjectNotFoundException("Film has been already deleted");
+        }
+        User user = userStorage.findUserById(userId);
+        Film film = filmStorage.findFilmById(filmId);
+        film.deleteLike(user);
+    }
+
+    @Override
+    public List<Film> getTopFilms(int count) {
+        return filmStorage.getFilmList().stream().sorted(Comparator.comparingInt(film -> film.getLikes().size()))
+                .limit(count)
+                .collect(Collectors.toList());
+
     }
 
     private int generateFilmId() {
