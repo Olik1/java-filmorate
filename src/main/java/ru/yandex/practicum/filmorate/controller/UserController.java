0d54a14f43d01d0a2425ba4f394d54d90736a50e
Olga <110121612+Olik1@users.noreply.github.com>
2023-04-10 16:52:09 +0300

    refactor: контроллер по работе с друзьями

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -2,9 +2,11 @@ package ru.yandex.practicum.filmorate.controller;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.service.UserService;
+import ru.yandex.practicum.filmorate.storage.UserStorage;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -18,19 +20,52 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public User addUser(@Valid @RequestBody User user) {
         log.info("Добавлен пользователь: {}", user);
         return userService.createUser(user);
     }
 
     @GetMapping
+    @ResponseStatus(HttpStatus.OK)
     public List<User> getUsers() {
         return userService.getAllUsers();
     }
 
     @PutMapping
+    @ResponseStatus(HttpStatus.OK)
     public User updateUser(@Valid @RequestBody User user) {
         log.info("Обновление данных пользователя: {}", user);
         return userService.updateUser(user);
+    }
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable int id) {
+       return userService.getUserById(id);
+    }
+    //PUT /users/{id}/friends/{friendId}  — добавление в друзья.
+    @PutMapping("/{id}/friends/{friendId}")
+    @ResponseStatus(HttpStatus.OK)
+    public void addFriends(@PathVariable int id, @PathVariable int friendId) {
+        log.info("Пользователь добавлен в друзья: {}", friendId);
+        userService.addFriend(id, friendId);
+    }
+    //DELETE /users/{id}/friends/{friendId} — удаление из друзей
+    @DeleteMapping("/{id}/friends/{friendId}")
+    @ResponseStatus(HttpStatus.OK)
+    public void deleteFriend(@PathVariable int id, @PathVariable int friendId) {
+        log.info("Пользователь был удален из списка друзей: {}", friendId);
+        userService.deleteFriendById(id, friendId);
+    }
+    //GET /users/{id}/friends
+    @GetMapping("/{id}/friends")
+    @ResponseStatus(HttpStatus.OK)
+    public List<User> getListOfFriends(@PathVariable int id) {
+        return userService.getListOfFriends(id);
+    }
+
+    @GetMapping("/{id}/friends/common/{otherId}")
+    @ResponseStatus(HttpStatus.OK)
+    public List<User> getListOfCommonFriends(@PathVariable int id, @PathVariable int friendId) {
+        return userService.getCommonFriends(id, friendId);
     }
 }
