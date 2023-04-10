22b03ee2ab0791611f24761bdfee0ecd1c5c7420
Olga <110121612+Olik1@users.noreply.github.com>
2023-04-10 20:55:41 +0300

    refactor: changes

--- src/test/java/ru/yandex/practicum/filmorate/FilmTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmTests.java
@@ -13,7 +13,6 @@ import ru.yandex.practicum.filmorate.storage.impl.InMemoryFilmStorage;
 import ru.yandex.practicum.filmorate.storage.impl.InMemoryUserStorage;
 
 import java.time.LocalDate;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -33,7 +32,7 @@ public class FilmTests extends FilmorateApplicationTests {
     }
 
     @BeforeEach
-    void BeforeEach() {
+    void beforeEach() {
         film1 = Film.builder()
                 .name("Архив Ланъя")
                 .description("Историческая дорама")
@@ -67,6 +66,7 @@ public class FilmTests extends FilmorateApplicationTests {
         assertNotNull(update, "Test error: film doesn't update!");
         assertEquals(actual1, update, "Test error: films aren't equals!");
     }
+
     @Test
     void shouldThrowNameIsEmptyTest() {
         film1.setName("");
@@ -97,12 +97,14 @@ public class FilmTests extends FilmorateApplicationTests {
         Exception exception = assertThrows(ValidationException.class, () -> filmController.addFilm(film1));
         assertEquals("MAX длина описания — 200 символов!", exception.getMessage());
     }
+
     @Test
     void shouldThrowReleaseDateTest() {
         film1.setReleaseDate(LocalDate.of(1800, 1, 1));
         Exception exception = assertThrows(ValidationException.class, () -> filmController.addFilm(film1));
         assertEquals("Дата релиза не может быть раньше 1895-12-28", exception.getMessage());
     }
+
     @Test
     void shouldThrowDurationNegativeTest() {
         film1.setDuration(-99);
--- src/test/java/ru/yandex/practicum/filmorate/UserTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/UserTests.java
@@ -29,7 +29,7 @@ public class UserTests extends FilmorateApplicationTests {
     }
 
     @BeforeEach
-    void BeforeEach() {
+    void beforeEach() {
         user1 = User.builder()
                 .email("Olga13@yandex.ru")
                 .login("Olik13")
