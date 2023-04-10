6c9082832b4e7b3c4f6e67151de610381516ee97
Olga <110121612+Olik1@users.noreply.github.com>
2023-04-10 20:41:14 +0300

    refactor: changes

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -6,7 +6,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.service.UserService;
-import ru.yandex.practicum.filmorate.storage.UserStorage;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -38,25 +37,29 @@ public class UserController {
         log.info("Обновление данных пользователя: {}", user);
         return userService.updateUser(user);
     }
+
     @GetMapping("/{id}")
     public User getUserById(@PathVariable int id) {
-       return userService.getUserById(id);
+        return userService.getUserById(id);
     }
-    //PUT /users/{id}/friends/{friendId}  — добавление в друзья.
+
+    //добавление в друзья.
     @PutMapping("/{id}/friends/{friendId}")
     @ResponseStatus(HttpStatus.OK)
     public void addFriends(@PathVariable int id, @PathVariable int friendId) {
         log.info("Пользователь добавлен в друзья: {}", friendId);
         userService.addFriend(id, friendId);
     }
-    //DELETE /users/{id}/friends/{friendId} — удаление из друзей
+
+    //удаление из друзей
     @DeleteMapping("/{id}/friends/{friendId}")
     @ResponseStatus(HttpStatus.OK)
     public void deleteFriend(@PathVariable int id, @PathVariable int friendId) {
         log.info("Пользователь был удален из списка друзей: {}", friendId);
         userService.deleteFriendById(id, friendId);
     }
-    //GET /users/{id}/friends
+
+    //возвращаем список пользователей
     @GetMapping("/{id}/friends")
     @ResponseStatus(HttpStatus.OK)
     public List<User> getListOfFriends(@PathVariable int id) {
