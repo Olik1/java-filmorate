6621a30c9c230d420e872e8ae755afcbc388db7d
Olga <110121612+Olik1@users.noreply.github.com>
2023-05-05 20:28:28 +0300

    style

--- src/main/java/ru/yandex/practicum/filmorate/storage/impl/InMemoryFilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/impl/InMemoryFilmStorage.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.filmorate.storage.impl;
 
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.exception.ObjectNotFoundException;
