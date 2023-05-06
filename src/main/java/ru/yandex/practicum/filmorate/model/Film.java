84d391c21dbb4128a32b418b981d2da9a5f43cf6
Olga <110121612+Olik1@users.noreply.github.com>
2023-05-06 22:13:03 +0300

    feat: добавлены поля рейтинг и жанры

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -3,7 +3,10 @@ package ru.yandex.practicum.filmorate.model;
 import lombok.Builder;
 import lombok.Data;
 
-import javax.validation.constraints.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
@@ -21,7 +24,10 @@ public class Film {
     private LocalDate releaseDate;
     @Positive
     private int duration;
+    @NotNull
+    private RatingMpa mpa;
     private final Set<Integer> likes = new HashSet<>();
+    private final Set<Genre> genres = new HashSet<>();
 
     public void addLike(User user) {
         likes.add(user.getId());
