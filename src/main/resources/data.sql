dfa880392bb6acc4e42d3f97d41646d2528b0b5a
Olga <110121612+Olik1@users.noreply.github.com>
2023-05-06 22:12:31 +0300

    changes

---
+++ src/main/resources/data.sql
@@ -0,0 +1,14 @@
+INSERT INTO Rating_MPA(RAITING_ID, MPA_NAME)
+VALUES (1, 'G'),
+       (2, 'PG'),
+       (3, 'PG-13'),
+       (4, 'R'),
+       (5, 'NC-17');
+
+INSERT INTO Genre (GENRE_ID, NAME)
+VALUES (1, 'Комедия'),
+       (2, 'Драма'),
+       (3, 'Мультфильм'),
+       (4, 'Документальный'),
+       (5, 'Боевик');
+
