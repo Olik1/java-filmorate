abd062575cab26904b9f9d657aeb883ad29fdf65
Olga <110121612+Olik1@users.noreply.github.com>
2023-05-09 20:48:41 +0300

    style: удалены комментарии, лишние отступы

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmGenreStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmGenreStorage.java
@@ -6,6 +6,9 @@ import java.util.Set;
 
 public interface FilmGenreStorage {
     Set<FilmGenre> getGenresByFilmId(int filmId);
+
     void deleteByFilmId(int filmId);
+
     FilmGenre add(FilmGenre filmGenre);
+
 }
